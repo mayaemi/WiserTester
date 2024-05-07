97c9599829372bc792dce068522b9e60854c8fa5
mayah <hoffmann.maya@gmail.com>
2024-05-07 16:05:46 +0300

    cleanup

--- wiser_tester.py
+++ wiser_tester.py
@@ -1,5 +1,5 @@
 import asyncio
-from src.configure import LOGGER, TestMode
+from src.configure import LOGGER
 from src.exceptions import handle_exceptions
 from src.compare import Compare
 from src.tester import WiserTester
